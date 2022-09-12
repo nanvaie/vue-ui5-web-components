c72f865856b4f46e368366245c1361cbf3e712d8
Mohammad-Reza Sharifi <mrsharifi76@gmail.com>
2022-09-12 20:40:41 +0430

    feat(VueComponents): Create "VSLabel" Vue Component based on "ui5-label" Web Component

---
+++ src/components/VSLabel.vue
@@ -0,0 +1,23 @@
+<template>
+    <ui5-label
+        :for="props.for"
+        :required="props.required === true ? true : null"
+        :show-colon="props.showColon === true ? true : null"
+        :wrapping-type="props.wrappingType === true ? true : null"
+    >
+        <slot></slot>
+    </ui5-label>
+</template>
+
+<script setup>
+
+import "@ui5/webcomponents/dist/Label";
+
+const props = defineProps({
+    for: String,
+    required: Boolean,
+    showColon: Boolean,
+    wrappingType: Boolean
+})
+
+</script>
