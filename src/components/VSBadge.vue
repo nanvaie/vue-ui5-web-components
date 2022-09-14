cebd5c3906685716dd00b71cb0ef563d1a700341
Mohammad-Reza Sharifi <mrsharifi76@gmail.com>
2022-09-14 12:23:44 +0430

    feat(VueComponents): Create "VSBadge" Vue Component based on "ui5-badge" Web Component

---
+++ src/components/VSBadge.vue
@@ -0,0 +1,25 @@
+<template>
+    <ui5-badge :color-scheme="props.colorScheme">
+        <slot></slot>
+        <div slot="icon">
+            <slot name="icon"></slot>
+        </div>
+    </ui5-badge>
+</template>
+
+<script setup>
+
+import "@ui5/webcomponents/dist/Badge";
+import {defineProps} from "vue";
+
+const props = defineProps({
+    colorScheme: {
+        type: String,
+        default: "1",
+        validator(value) {
+            return value >= "1" && value <= "10";
+        }
+    }
+});
+
+</script>
