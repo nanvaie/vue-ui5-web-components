ec2d3fc6b32e3a20bf5705ae55c71bacc86573b5
Mohammad-Reza Sharifi <mrsharifi76@gmail.com>
2022-09-15 22:58:50 +0430

    feat(VueComponents): Create "VSBar" Vue Component based on "ui5-bar" Web Component

---
+++ src/components/VSBar.vue
@@ -0,0 +1,34 @@
+<template>
+    <ui5-bar :design="props.design">
+        <div slot="startContent">
+            <slot name="startContent"></slot>
+        </div>
+
+        <slot></slot>
+
+        <div slot="endContent">
+            <slot name="endContent"></slot>
+        </div>
+    </ui5-bar>
+</template>
+
+<script setup>
+
+import "@ui5/webcomponents-fiori/dist/Bar";
+import { defineProps } from "vue";
+
+const props = defineProps({
+    design: {
+        type: String,
+        validator(value) {
+            return [
+                "Header",
+                "Subheader",
+                "Footer",
+                "FloatingFooter",
+            ].includes(value);
+        }
+    }
+});
+
+</script>
