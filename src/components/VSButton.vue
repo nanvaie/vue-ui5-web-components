f3442a760c146c2b0cddd3279eaaad5b838db1f6
Mohammad-Reza Sharifi <mrsharifi76@gmail.com>
2022-09-15 23:14:33 +0430

    feat(VueComponents): Create "VSButton" Vue Component based on "ui5-button" Web Component

---
+++ src/components/VSButton.vue
@@ -0,0 +1,55 @@
+<template>
+    <ui5-button
+        :accessibility-attributes="props.accessibilityAttributes"
+        :accessible-name="props.accessibleName"
+        :accessible-name-ref="props.accessibleNameRef"
+        :design="props.design"
+        :disabled="props.disabled"
+        :icon="props.icon"
+        :icon-end="props.iconEnd"
+        :submits="props.submits"
+        :tooltip="props.tooltip"
+    >
+        <slot></slot>
+    </ui5-button>
+</template>
+
+<script setup>
+
+import "@ui5/webcomponents/dist/Button";
+import { defineProps } from "vue";
+
+const props = defineProps({
+    accessibilityAttributes: Object,
+    accessibleName: String,
+    accessibleNameRef: String,
+    design: {
+        type: String,
+        validator(value) {
+            return [
+                "Default",
+                "Emphasized",
+                "Positive",
+                "Negative",
+                "Transparent",
+                "Attention",
+            ].includes(value);
+        }
+    },
+    disabled: {
+        type: Boolean,
+        default: undefined
+    },
+    icon: String,
+    iconEnd: {
+        type: Boolean,
+        default: undefined
+    },
+    submits: {
+        type: Boolean,
+        default: undefined
+    },
+    tooltip: String
+});
+
+</script>
