fc3824138a05f257f3281eb38ee35d567da4a0a2
Mohammad-Reza Sharifi <mrsharifi76@gmail.com>
2022-09-28 23:53:37 +0330

    feat(VueComponents): Create "VSCheckBox" Vue Component based on "ui5-checkbox" Web Component

---
+++ src/components/VSCheckBox.vue
@@ -0,0 +1,72 @@
+<template>
+    <ui5-checkbox
+        :accessible-name="props.accessibleName"
+        :accessible-name-ref="props.accessibleNameRef"
+        :checked="props.checked"
+        :disabled="props.disabled"
+        :indeterminate="props.indeterminate"
+        :name="props.name"
+        :readonly="props.readonly"
+        :required="props.required"
+        :text="props.text"
+        :value-state="props.valueState"
+        :wrapping-type="props.wrappingType"
+    >
+        <slot></slot>
+    </ui5-checkbox>
+</template>
+
+<script setup>
+
+import "@ui5/webcomponents/dist/CheckBox";
+import { defineProps } from "vue";
+
+const props = defineProps({
+    accessibleName: String,
+    accessibleNameRef: String,
+    checked: {
+        type: Boolean,
+        default: undefined
+    },
+    disabled: {
+        type: Boolean,
+        default: undefined
+    },
+    indeterminate: {
+        type: Boolean,
+        default: undefined
+    },
+    name: String,
+    readonly: {
+        type: Boolean,
+        default: undefined
+    },
+    required: {
+        type: Boolean,
+        default: undefined
+    },
+    text: String,
+    valueState: {
+        type: String,
+        validator(value) {
+            return [
+                "Warning",
+                "Error",
+                "None",
+                "Success",
+                "Information",
+            ].includes(value);
+        }
+    },
+    wrappingType: {
+        type: String,
+        validator(value) {
+            return [
+                "None",
+                "Normal",
+            ].includes(value);
+        }
+    }
+});
+
+</script>
