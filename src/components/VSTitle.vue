48b5bba9a6113cd5b90985dc0c3bc2d8a489e97d
Mohammad-Reza Sharifi <mrsharifi76@gmail.com>
2022-09-28 23:53:16 +0330

    feat(VueComponents): Create "VSTitle" Vue Component based on "ui5-title" Web Component

---
+++ src/components/VSTitle.vue
@@ -0,0 +1,40 @@
+<template>
+    <ui5-title
+        :level="props.level"
+        :wrapping-type="props.wrappingType"
+    >
+        <slot></slot>
+    </ui5-title>
+</template>
+
+<script setup>
+
+import "@ui5/webcomponents/dist/Title";
+import { defineProps } from "vue";
+
+const props = defineProps({
+    level: {
+        type: String,
+        validator(value) {
+            return [
+                "H1",
+                "H2",
+                "H3",
+                "H4",
+                "H5",
+                "H6",
+            ].includes(value);
+        }
+    },
+    wrappingType: {
+        type: String,
+        validator(value) {
+            return [
+                "None",
+                "Normal"
+            ].includes(value);
+        }
+    }
+});
+
+</script>
