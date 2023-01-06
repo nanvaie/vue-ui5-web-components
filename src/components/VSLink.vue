80c559904b9f1e6c3b3157b0d4ac965a56647e22
Mohammad-Reza Sharifi <mrsharifi76@gmail.com>
2023-01-06 17:48:42 +0330

    feat(VueComponents): Add VSLink.vue

---
+++ src/components/VSLink.vue
@@ -0,0 +1,78 @@
+<template>
+    <ui5-link
+        :accessibility-attributes="props.accessibilityAttributes"
+        :accessible-name="props.accessibleName"
+        :accessible-name-ref="props.accessibleNameRef"
+        :accessible-role="props.accessibleRole"
+        :design="props.design"
+        :disabled="props.disabled"
+        :href="props.href"
+        :target="props.target"
+        :wrapping-type="props.wrappingType"
+    >
+        <slot />
+    </ui5-link>
+</template>
+
+<script setup>
+
+import "@ui5/webcomponents/dist/Link";
+import LinkDesign from "@ui5/webcomponents/src/types/LinkDesign";
+import WrappingType from "@ui5/webcomponents/src/types/WrappingType";
+
+const props = defineProps({
+    accessibilityAttributes: Object,
+    accessibleName: String,
+    accessibleNameRef: String,
+    accessibleRole: {
+        type: String,
+        default: 'link',
+        validator(value) {
+            return [
+                'link',
+                'button',
+            ].includes(value);
+        },
+    },
+    design: {
+        type: String,
+        default: LinkDesign.Default,
+        validator(value) {
+            return [
+                LinkDesign.Default,
+                LinkDesign.Subtle,
+                LinkDesign.Emphasized,
+            ].includes(value);
+        },
+    },
+    disabled: {
+        type: Boolean,
+        default: undefined,
+    },
+    href: String,
+    target: {
+        type: String,
+        default: undefined,
+        validator(value) {
+            return [
+                '_self',
+                '_top',
+                '_blank',
+                '_parent',
+                '_search',
+            ].includes(value);
+        },
+    },
+    wrappingType: {
+        type: String,
+        default: WrappingType.None,
+        validator(value) {
+            return [
+                WrappingType.None,
+                WrappingType.Normal,
+            ].includes(value);
+        },
+    }
+});
+
+</script>
