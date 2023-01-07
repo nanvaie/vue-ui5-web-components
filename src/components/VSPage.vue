3870deba805086d8961cd13e9bb0e1999a64729b
Mohammad-Reza Sharifi <mrsharifi76@gmail.com>
2023-01-07 16:27:50 +0330

    feat(VueComponents): Add VSPage

---
+++ src/components/VSPage.vue
@@ -0,0 +1,52 @@
+<template>
+    <ui5-page>
+        <div
+            v-if="$slots.header"
+            slot="header"
+        >
+            <slot name="header" />
+        </div>
+
+        <slot />
+
+        <div
+            v-if="$slots.footer"
+            slot="footer"
+        >
+            <slot name="footer" />
+        </div>
+    </ui5-page>
+</template>
+
+<script setup>
+
+import '@ui5/webcomponents-fiori/dist/Page';
+import PageBackgroundDesign from '@ui5/webcomponents-fiori/dist/types/PageBackgroundDesign';
+
+const props = defineProps({
+    backgroundDesign: {
+        type: String,
+        default: PageBackgroundDesign.Solid,
+        validator(value) {
+            return [
+                PageBackgroundDesign.Solid,
+                PageBackgroundDesign.Transparent,
+                PageBackgroundDesign.List,
+            ].includes(value);
+        },
+    },
+    disableScrolling: {
+        type: Boolean,
+        default: undefined,
+    },
+    floatingFooter: {
+        type: Boolean,
+        default: undefined,
+    },
+    hideFooter: {
+        type: Boolean,
+        default: undefined,
+    },
+});
+
+</script>
